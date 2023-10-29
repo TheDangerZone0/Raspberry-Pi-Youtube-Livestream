99c9afb2704496e3c4e8ba452ec0deca3cfda20b
John Reynolds <130010986+TheDangerZone1000@users.noreply.github.com>
2023-10-29 16:08:33 -0700

    Create 720p-livestream.py

---
+++ 60fps/720p-livestream.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("libcamera-vid --inline --nopreview -t 0 --width 1280 --height 720 --framerate 30 --codec h264 -o - | ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -fflags nobuffer -thread_queue_size 1024 -use_wallclock_as_timestamps 1 -i pipe:0 -c:a aac -preset fast -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/3a0g-ey1b-dgt4-mbpm-fzfx")
